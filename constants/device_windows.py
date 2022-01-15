b215c795a9d3818cffbb9fd3df87a6d129720b5b
Virts <virtsnick@gmail.com>
2022-01-15 16:28:02 +0200

    add device window for more models

--- constants/device_windows.py
+++ constants/device_windows.py
@@ -2,13 +2,6 @@ from constants.enumerates import ElectricalAppliances
 
 
 WINDOWS = {
-    'VIBFNET': {
-        ElectricalAppliances.MICROWAVE: 50,
-        ElectricalAppliances.KETTLE: 50,
-        ElectricalAppliances.FRIDGE: 350,
-        ElectricalAppliances.WASHING_MACHINE: 150,
-        ElectricalAppliances.DISH_WASHER: 450,
-    },
     'FNET': {
         ElectricalAppliances.MICROWAVE: 50,
         ElectricalAppliances.KETTLE: 50,
@@ -43,5 +36,47 @@ WINDOWS = {
         ElectricalAppliances.FRIDGE: 400,
         ElectricalAppliances.WASHING_MACHINE: 200,
         ElectricalAppliances.DISH_WASHER: 450,
+    },
+    'DAE': {
+        ElectricalAppliances.MICROWAVE: 100,
+        ElectricalAppliances.KETTLE: 300,
+        ElectricalAppliances.FRIDGE: 400,
+        ElectricalAppliances.WASHING_MACHINE: 400,
+        ElectricalAppliances.DISH_WASHER: 500,
+    },
+    'VAE': {
+        ElectricalAppliances.MICROWAVE: 100,
+        ElectricalAppliances.KETTLE: 300,
+        ElectricalAppliances.FRIDGE: 400,
+        ElectricalAppliances.WASHING_MACHINE: 400,
+        ElectricalAppliances.DISH_WASHER: 500,
+    },
+    'VIB_SAED': {
+        ElectricalAppliances.MICROWAVE: 100,
+        ElectricalAppliances.KETTLE: 50,
+        ElectricalAppliances.FRIDGE: 250,
+        ElectricalAppliances.WASHING_MACHINE: 50,
+        ElectricalAppliances.DISH_WASHER: 200,
+    },
+    'VIBFNET': {
+        ElectricalAppliances.MICROWAVE: 50,
+        ElectricalAppliances.KETTLE: 50,
+        ElectricalAppliances.FRIDGE: 350,
+        ElectricalAppliances.WASHING_MACHINE: 150,
+        ElectricalAppliances.DISH_WASHER: 450,
+    },
+    'VIBSeq2Point': {
+        ElectricalAppliances.MICROWAVE: 100,
+        ElectricalAppliances.KETTLE: 300,
+        ElectricalAppliances.FRIDGE: 400,
+        ElectricalAppliances.WASHING_MACHINE: 400,
+        ElectricalAppliances.DISH_WASHER: 500,
+    },
+    'VIBWGRU': {
+        ElectricalAppliances.MICROWAVE: 100,
+        ElectricalAppliances.KETTLE: 150,
+        ElectricalAppliances.FRIDGE: 450,
+        ElectricalAppliances.WASHING_MACHINE: 150,
+        ElectricalAppliances.DISH_WASHER: 350,
     },
 }
